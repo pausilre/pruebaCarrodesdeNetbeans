fcc5c8fb5146468cb883fc65fc2236cf220262b0
Paula Silvestre <pausilre@gmail.com>
2022-01-17 09:12:49 +0100

    Añadido compratest desde netbeans

---
+++ test/CompraTest.java
@@ -0,0 +1,54 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author paula
+ * @version 13.02
+ */
+public class CompraTest {
+    
+    public CompraTest() {
+    }
+    
+    /**
+     * Test of getPantalones method, of class Compra.
+     */
+    @Test
+    public void testGetPantalones() {
+        System.out.println("getPantalones");
+        Compra carrito1 = new Compra();
+        int expResult = 0;
+        int result = carrito1.getPantalones();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getCamisas method, of class Compra.
+     */
+    @Test
+    public void testGetCamisas() {
+        System.out.println("getCamisas");
+        Compra carrito2 = new Compra();
+        carrito2.realizarCompra();
+        carrito2.realizarCompra();
+        assertEquals(carrito2.getCamisas(), 2);
+    }
+
+    /**
+     * Test of getSudaderas method, of class Compra.
+     */
+    @Test
+    public void testGetSudaderas() {
+        System.out.println("getSudaderas");
+        Compra carrito3 = new Compra();
+        carrito3.realizarCompra();
+        carrito3.realizarCompra();
+        carrito3.realizarCompra();
+        int expResult = 3;
+        int result = carrito3.getSudaderas();
+        assertEquals(expResult, result);
+    }
+    
+}
