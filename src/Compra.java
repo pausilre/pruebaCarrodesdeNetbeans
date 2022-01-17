fe59be877e080fae798ca0dd8027eb48320ebbe9
Paula Silvestre <pausilre@gmail.com>
2022-01-17 11:06:40 +0100

    añadidos comentarios para javadoc

--- src/Compra.java
+++ src/Compra.java
@@ -5,28 +5,54 @@
  */
 
 /**
- *
+ *La clase compra sirve para realizar una lista de la compra
  * @author paula
+ * @version 13.02
  */
 public class Compra {
     private int pantalones;
 	private int camisas;
 	private int sudaderas;
 	
+        /**
+         * El metodo realizarCompra incrementa la compra en 1 producto de cada
+         */
 	public void realizarCompra() {
 		pantalones++;
 		camisas++;
 		sudaderas++;
 	}
-	
+	/**
+         * El metodo getPantalones te devuelve la cantidad de pantalones
+         * @return numero de pantalones
+         */
 	public int getPantalones() {
 		return pantalones;
 	}
+        /**
+         * El metodo getCamisas te devuelve la cantidad de camisas
+         * @return numero de camisas
+         */
 	public int getCamisas() {
 		return camisas;
 	}
+        
+        /**
+         * El metodo getSudaderas te devuelve la cantidad de sudaderas
+         * @return numero de sudaderas
+         */
 	public int getSudaderas() {
 		return sudaderas;
 	}
+        
+        /**
+         * El metodo sumaCamisasyPantalones devuelve la suma entre pantalones y camisas
+         * @param pantalones numero de pantalones
+         * @param camisas numero de camisas
+         * @return total de la suma entre pantalones y camisas
+         */
+        public int sumaCamisasyPantalones(int pantalones, int camisas){
+            return pantalones+camisas;
+        }
     
 }
